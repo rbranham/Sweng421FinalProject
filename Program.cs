e75f303a9bd4649d0651883355280de24079f275
rbranham <roger.brnhm@gmail.com>
2020-12-09 15:34:34 -0600

    Wrote Object structures for Quiz/Qeustions and Results

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -19,85 +20,113 @@ namespace Sweng421FinalProject
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
         }
-    }
-    public interface QuizIF
-    {
 
-    }
-    public interface QuizTypeIF
-    {
+        /*
+         * Interface for questions 
+        */
+        interface QuestionIF {
 
-    }
-    public interface ResultsIF
-    {
-        public float getScore()
-        {
-            return 0;
+            public ResultsIF runQuiz();  //Run from student portal will call specific pop up and then return results
+            //Should evenetualy return a results interface
+
+            public void createQuiz();  //Run from teacher portal to create question
+            //Maybe just run in constructor?
+
+            public ResultsIF generateAnswerKey(); //Run to generate a ResultsIF with the correct answer.   
+            //we may want to generate on creation, not sure. 
         }
-        public void seeResults()
+
+
+        class Quiz : QuestionIF
         {
+            List<QuestionIF> subQuestions;
+            
+            
+
+            public void createQuiz()
+            {
+                throw new NotImplementedException();
+
+                //Launch creation interface dialog
+                //returns a list of subQuestions
+                //set returned list to internal list
+
+            }
+
+            public ResultsIF generateAnswerKey()
+            {
+                throw new NotImplementedException();
+            }
+
+            public ResultsIF runQuiz()
+            {
+                ResultDecoratorForComposites results = new ResultDecoratorForComposites();
+
+                //Loop through each question calling and returning
+                for (int i = 0; i < subQuestions.Count(); i++)
+                {
+                    results.addResult( subQuestions[i].runQuiz()); //Add each answer to list
+                }
+
+                return results; //Return the Results
+            }
+        }
+
+        //Stubbed
+        class MultipleChoiceQuestion : QuestionIF
+        {
+            public void createQuiz()
+            {
+                throw new NotImplementedException();
+            }
+
+            public ResultsIF generateAnswerKey()
+            {
+                throw new NotImplementedException();
+            }
+
+            public ResultsIF runQuiz()
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+
+        interface ResultsIF {
+
+            public void writeToFile(String fileString); //Writes the results to a file specified by string
 
         }
-    }
-    public class QuizCompositie : QuizIF
-    {
-        List<QuizIF> questions = new List<QuizIF>();
-        QuizTypeIF type;
-        ResultsIF result;
-        public QuizIF getQuestion(int index)
+
+        class ResultDecoratorForComposites : ResultsIF
         {
-            return questions[index]; //place holder
+            List<ResultsIF> rList;
+
+            public ResultDecoratorForComposites()
+            {
+                rList = new List<ResultsIF>(); //Init
+            }
+
+            public void writeToFile(string fileString)
+            {
+                throw new NotImplementedException();
+                //Open file
+                //loop through the results list
+                    //Call the write function for each element in list
+            }
+
+           public void addResult(ResultsIF r)
+            {
+                rList.Add(r);  //Add to list
+            }
+
         }
-        public ResultsIF gradeQuiz()
-        {
-            return result;
-        }
-    }
-    public class StandardResults : ResultsIF
-    {
-        public float Score;
+
+
+
+
+
 
     }
-    public abstract class AdvancedResultsAC : ResultsIF
-    {
-        public ResultsIF standardScore;
-
-    }
-    public class FeedbackResults : AdvancedResultsAC
-    {
-        public float Score;
-        public String feedback = "";
-
-    }
-    public class QuizFactory
-    {
-        public QuizIF createQuiz(QuizIF qtype)
-        {
-            return qtype;
-        }
-    }
-    public class Weekly : QuizTypeIF
-    {
-
-    }
-    public class MidTerm : QuizTypeIF
-    {
-
-    }
-    public class Exam : QuizTypeIF
-    {
-
-    }
-    public class FIB_Q : QuizIF
-    {
-
-    }
-    public class SA_Q : QuizIF
-    {
-
-    }
-    public class MC_Q : QuizIF
-    {
-
-    }
+ 
 }
