5201db636d2b382e5cf172f598b30889518a091c
rbranham <roger.brnhm@gmail.com>
2020-12-09 19:16:45 -0600

    Added sheel form and logic for creation of a MC question

---
+++ MultipleChoiceCreation.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Sweng421FinalProject
+{
+    partial class MultipleChoiceCreation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelQuestion = new System.Windows.Forms.Label();
+            this.textBoxQuestion = new System.Windows.Forms.TextBox();
+            this.buttonDone = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelQuestion
+            // 
+            this.labelQuestion.AutoSize = true;
+            this.labelQuestion.Location = new System.Drawing.Point(33, 21);
+            this.labelQuestion.Name = "labelQuestion";
+            this.labelQuestion.Size = new System.Drawing.Size(61, 15);
+            this.labelQuestion.TabIndex = 0;
+            this.labelQuestion.Text = "Question: ";
+            // 
+            // textBoxQuestion
+            // 
+            this.textBoxQuestion.Location = new System.Drawing.Point(149, 21);
+            this.textBoxQuestion.Name = "textBoxQuestion";
+            this.textBoxQuestion.Size = new System.Drawing.Size(268, 23);
+            this.textBoxQuestion.TabIndex = 1;
+            // 
+            // buttonDone
+            // 
+            this.buttonDone.Location = new System.Drawing.Point(701, 403);
+            this.buttonDone.Name = "buttonDone";
+            this.buttonDone.Size = new System.Drawing.Size(75, 23);
+            this.buttonDone.TabIndex = 2;
+            this.buttonDone.Text = "Save";
+            this.buttonDone.UseVisualStyleBackColor = true;
+            this.buttonDone.Click += new System.EventHandler(this.buttonDone_Click);
+            // 
+            // MultipleChoiceCreation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonDone);
+            this.Controls.Add(this.textBoxQuestion);
+            this.Controls.Add(this.labelQuestion);
+            this.Name = "MultipleChoiceCreation";
+            this.Text = "MultipleChoiceCreation";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelQuestion;
+        private System.Windows.Forms.TextBox textBoxQuestion;
+        private System.Windows.Forms.Button buttonDone;
+    }
+}
---
+++ MultipleChoiceCreation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sweng421FinalProject
+{
+    public partial class MultipleChoiceCreation : Form
+    {
+        //Variables to save 
+        public String question;
+
+
+        public MultipleChoiceCreation()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDone_Click(object sender, EventArgs e)
+        {
+            question = textBoxQuestion.Text; //Get Question text
+        }
+    }
+}
