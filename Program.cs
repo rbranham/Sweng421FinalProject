2853aff89dbcb6e4771f4251f5d1830e0d18fb19
rbranham <roger.brnhm@gmail.com>
2020-12-10 11:36:34 -0600

    Refactored Results classes into their own file.

--- Program.cs
+++ Program.cs
@@ -24,34 +24,4 @@ namespace Sweng421FinalProject
     }
 
 
-    public interface ResultsIF
-    {
-
-        public void writeToFile(String fileString); //Writes the results to a file specified by string
-
-    }
-
-    public class ResultDecoratorForComposites : ResultsIF
-    {
-        List<ResultsIF> rList;
-
-        public ResultDecoratorForComposites()
-        {
-            rList = new List<ResultsIF>(); //Init
-        }
-
-        public void writeToFile(string fileString)
-        {
-            throw new NotImplementedException();
-            //Open file
-            //loop through the results list
-            //Call the write function for each element in list
-        }
-
-        public void addResult(ResultsIF r)
-        {
-            rList.Add(r);  //Add to list
-        }
-
-    }
 }
