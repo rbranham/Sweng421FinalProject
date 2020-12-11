842d29f12071d93f15174143f7434539aa1cc595
rbranham <roger.brnhm@gmail.com>
2020-12-10 20:56:34 -0600

    Cleaned up old database Systems

--- DBhandler.cs
+++ DBhandler.cs
@@ -8,14 +8,9 @@ using Google.Cloud.Firestore;
 
 namespace Sweng421FinalProject
 {
-    //Class is set up as a singleton just for easier data persitance in testing database. Would not be singleton with real database
+
     class DBhandler
     {
-        //List<String> teachID = new List<String> { "12345", "23456", "34567", "45678", "56789", "67890" };
-        //List<String> studentID = new List<String> { "9876", "8765", "7654", "6543", "5432", "4321" };
-
-        String dbFolderString = @"..\..\..\DatabaseFolder\" ;
-
         private static DBhandler INSTANCE = null;
         FirestoreDb db; 
         public DBhandler()
@@ -24,9 +19,9 @@ namespace Sweng421FinalProject
         }
 
         
-        public static DBhandler getInstance()
+        public static DBhandler getInstance() //This doesn't have to be a singleton anymore with new database. 
         {
-            if (INSTANCE == null)  //Create handler if doesn't exist
+            if (INSTANCE == null) 
             {
                 INSTANCE = new DBhandler(); 
             }
@@ -71,41 +66,6 @@ namespace Sweng421FinalProject
 
             return q;
         }
-
-        //With a real database would make sql calls inside these methods instead of reading files
-
-        /*
-        public List<String> getTeacherAccounts() {
-            List<String> t = new List<String>();
-
-            Debug.WriteLine("Accessing Teacher Accounts"); 
-
-            foreach (String line in File.ReadLines(dbFolderString + "teacher_accounts.txt", Encoding.UTF8))
-            {
-                //add lines to list view
-                Debug.WriteLine("Added Teacher Account: " + line);
-                t.Add(line);
-            }
-
-            return t;
-
-        }  
-
-        public List<String> getStudentAccounts() {
-            List<String> s = new List<String>();
-
-            Debug.WriteLine("Accessing Student Accounts");
-
-            foreach (String line in File.ReadLines(dbFolderString + "student_accounts.txt", Encoding.UTF8))
-            {
-                //add lines to list view
-                Debug.WriteLine("Added Student Account: " + line);
-                s.Add(line);
-            }
-
-            return s;
-        }  
-        */
 
         public async Task<List<String>> getQuestionTypes()
         {
