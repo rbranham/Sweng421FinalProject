bf9a7f8d407eefc9381e6e74782007f0ee4b9db2
jlstas <56229045+jlstas@users.noreply.github.com>
2020-12-11 15:26:24 -0500

    Update MC.cs

--- MC.cs
+++ MC.cs
@@ -21,12 +21,20 @@ namespace Sweng421FinalProject
             {
                 return (String)textBox1.Text;
             }
+            set
+            {
+                textBox1.Text = null;
+            }
         }
         public String addAnswer1
         {
             get
             {
                 return (String)textBox2.Text;
+            }
+            set
+            {
+                textBox2.Text = null;
             }
         }
         public String addAnswer2
@@ -35,6 +43,10 @@ namespace Sweng421FinalProject
             {
                 return (String)textBox3.Text;
             }
+            set
+            {
+                textBox3.Text = null;
+            }
         }
         public String addAnswer3
         {
@@ -42,12 +54,20 @@ namespace Sweng421FinalProject
             {
                 return (String)textBox4.Text;
             }
+            set
+            {
+                textBox4.Text = null;
+            }
         }
         public String addAnswer4
         {
             get
             {
                 return (String)textBox5.Text;
+            }
+            set
+            {
+                textBox5.Text = null;
             }
         }
     }
